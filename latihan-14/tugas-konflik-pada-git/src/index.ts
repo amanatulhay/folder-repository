f255b35d507f0bed681d7ad3aab63be24454753c
amanatulhay <amanatulhay.pribadi@gmail.com>
2024-12-18 22:32:51 +0800

    Add product management endpoints

--- latihan-14/tugas-konflik-pada-git/src/index.ts
+++ latihan-14/tugas-konflik-pada-git/src/index.ts
@@ -15,32 +15,18 @@ async function init() {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    // Endpoint untuk register user
-    app.post('/register', (req: Request, res: Response) => {
-      const { username, password } = req.body;
-      // Logika untuk register user
-      res.send('User registered');
-    });
-
-    // Endpoint untuk login user
-    app.post('/login', (req: Request, res: Response) => {
-      const { username, password } = req.body;
-      // Logika untuk login user
-      res.send('User logged in');
-    });
-
     // Endpoint untuk menambah produk
-    app.post('/products', (req: Request, res: Response) => {
-    const { name, price } = req.body;
-    // Logika untuk menambah produk
-    res.send('Product added');
-    });
+app.post('/products', (req: Request, res: Response) => {
+  const { name, price } = req.body;
+  // Logika untuk menambah produk
+  res.send('Product added');
+});
 
-    // Endpoint untuk mendapatkan semua produk
-    app.get('/products', (req: Request, res: Response) => {
-    // Logika untuk mendapatkan semua produk
-    res.send('List of products');
-    });
+// Endpoint untuk mendapatkan semua produk
+app.get('/products', (req: Request, res: Response) => {
+  // Logika untuk mendapatkan semua produk
+  res.send('List of products');
+});
 
     app.use("/api", routes);
 
